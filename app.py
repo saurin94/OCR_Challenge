5ff49088d65ab96bd5c4054584febc11ea71b8a2
Saurin Shah <saurin@Saurins-MacBook-Pro.local>
2017-10-22 01:22:15 -0400

    changes

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for
+import webbrowser
+app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
-
 def index():
 	return render_template('index.html')
 
-@app.route('/image_click_button',methods = ['POST', 'GET'])
+@app.route('/image_click_button', methods = ['POST','GET'])
 def image_click_button():
-   if request.method == 'POST':
-      result = request.form
-      print result
-      print "Hello"
+	print "in click button"
+	result = request.form
+	if request.method == 'POST':
+		return render_template("clicked.html", something="saurin")
+	#return render_template('index.html', something="saurin did it")
 
 app.run(debug=True)
 
